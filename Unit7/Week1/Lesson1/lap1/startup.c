392cb2985e48654df75353a02efd74ad595d5b44
abdelrhman marzoq <abdelrahmann595@gmail.com>
2024-07-19 20:49:27 +0300

    Progress

---
+++ Unit7/Week1/Lesson1/lap1/startup.c
@@ -0,0 +1,71 @@
+extern unsigned int STACK_TOP;
+extern unsigned int END_TEXT;
+extern unsigned int START_DATA;
+extern unsigned int END_DATA;
+extern unsigned int START_BSS;
+extern unsigned int END_BSS;
+
+
+extern int main();
+
+void RESET_HANDLER(){
+	
+
+	// cpy data section from flash to sram
+	unsigned int DATA_SIZE  = (unsigned char *)&END_DATA - (unsigned char *)&START_DATA;
+	unsigned char *data_src = (unsigned char *)&END_TEXT;
+	unsigned char *data_dst = (unsigned char *)&START_DATA;
+	for (int i = 0; i < DATA_SIZE; i++)
+	{
+		*((unsigned char *)data_dst++) = *((unsigned char *)data_src++);
+	}
+
+
+	unsigned int BSS_SIZE  = (unsigned char *)&END_BSS - (unsigned char *)&START_BSS;
+	unsigned char *bss_dst = (unsigned char *)&END_DATA;
+	for (int i = 0; i < BSS_SIZE; i++)
+	{
+		*((unsigned char *)bss_dst++) = (unsigned char)10;
+	}
+
+	// jump to main()
+	main();
+
+}
+
+void NMI_HANDLER(){
+	RESET_HANDLER();
+
+}
+
+void H_FAULT_HANDLER(){
+	RESET_HANDLER();
+
+}
+
+void MM_FAULT_HANDLER(){
+	RESET_HANDLER();
+
+}
+
+void BUS_FAULT_HANDLER(){
+	RESET_HANDLER();
+
+}
+
+void USAGE_FAULT_HANDLER(){
+	RESET_HANDLER();
+
+}
+
+
+unsigned int vectors[] __attribute__((section(".vectors"))) = {
+	(unsigned int)&STACK_TOP,
+	(unsigned int)&RESET_HANDLER,
+	(unsigned int)&NMI_HANDLER,
+	(unsigned int)&H_FAULT_HANDLER,
+	(unsigned int)&MM_FAULT_HANDLER,
+	(unsigned int)&BUS_FAULT_HANDLER,
+	(unsigned int)&USAGE_FAULT_HANDLER
+};
+
